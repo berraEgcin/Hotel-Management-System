6c6ee519dce141a257058b1f72f12564def9723d
Berra Eğcin <berraegcin@gmail.com>
2024-12-15 12:03:39 +0300

    Add the password later

--- hotel_management_system_db/src/main/java/org/example/DatabaseConnection.java
+++ hotel_management_system_db/src/main/java/org/example/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/hotel_management_system1";
         String username = "root";
-        String password = "NZHBCt5*";
+        String password = "";
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connection successful!");
